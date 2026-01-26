9c66fbe351b953fcfe46dcddf35b4c71a9ce8052
Alix Crusoe <acrusoe@f4r13s12.clusters.42paris.fr>
2026-01-26 16:54:53 +0100

    ex02 suite

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -8,21 +8,21 @@
 #include <utility>
 #include "PmergeMe.hpp"
 
-int findPosition(vector<int>const& chaine, int valeurPetite, int positionValeurGrande)
-{
-	int gauche = 0;
-	int droite = positionValeurGrande;
+// int findPosition(vector<int>const& chaine, int valeurPetite, int positionValeurGrande)
+// {
+// 	int gauche = 0;
+// 	int droite = positionValeurGrande;
 
-	while (gauche < droite)
-	{
-		int milieu = gauche + (droite - gauche) / 2;
-		if (chaine[milieu] < valeurPetite)
-			gauche = milieu + 1;
-		else 
-			droite = milieu;
-	}
-	return (gauche);
-}
+// 	while (gauche < droite)
+// 	{
+// 		int milieu = gauche + (droite - gauche) / 2;
+// 		if (chaine[milieu] < valeurPetite)
+// 			gauche = milieu + 1;
+// 		else 
+// 			droite = milieu;
+// 	}
+// 	return (gauche);
+// }
 
 bool comparePairs(Pair const& a, Pair const& b)
 {
@@ -34,8 +34,9 @@ int	main(int ac, char**av)
 	int i = 1;
 	int orphan = 0;
 	std::vector<Pair>A;
-	std::vector<int>B;
-	std::vector<int>main_chain;
+	std::vector<Pair>main_chain;
+	std::vector<int>smaller_chain;
+	std::vector<int>greater_chain;
 	std::vector<int>tmp;
 	size_t j = 0;
 	std::vector<int>nb;
@@ -70,16 +71,23 @@ int	main(int ac, char**av)
 	j = 0;
 	while (j < A.size())
 	{
-		main_chain.push_back(A[j].greater);
-		B.push_back(A[j].smaller);
+		greater_chain.push_back(A[j].greater);
+		smaller_chain.push_back(A[j].smaller);
 		j++;
 	}
-	// main_chain.insert(main_chain.begin(), B[0]);
-	// B.insert(B.begin(), B[0]);
 	j = 0;
-	while (j < main_chain.size())
+	while (j < smaller_chain.size())
 	{
-		std::cout << main_chain[j] << std::endl;
+		int value = smaller_chain[index[j]];
+		// chercher position de l'autre element de la paire dans main_chain(greater_chain[index[j]])
+		int pos = findPosition(main_chain, value, pos_greater);
+		main_chain.insert(main_chain.begin() + pos, value);
+		j++;
+	}
+	j = 0;
+	while (j < greater_chain.size())
+	{
+		std::cout << greater_chain[j] << std::endl;
 		j++;
 	}
 	j = 0;
